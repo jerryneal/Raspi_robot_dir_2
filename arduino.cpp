25afcc08ea08978dd9d52af7445f9ec43edaf238
Neal Bazirake <Kanyu@Euphoria.local>
2015-10-29 19:36:03 -0400

    Changing to working version
    This version works with all 8 motors on it.

--- arduino.cpp
+++ arduino.cpp
@@ -15,7 +15,8 @@ Servo myServo8;
 int angle,i;
 int maxAngle = 180;
 Servo array[] = {myServo1,myServo2,myServo3,myServo4,myServo5,myServo6,myServo7,myServo8};
-  
+Servo lefthandarray[] = {myServo1,myServo2,myServo3,myServo4};
+Servo righthandarray[] = {myServo5,myServo6,myServo7,myServo8};
 void setup() 
 {
   
@@ -23,30 +24,46 @@ void setup()
   myServo2.attach(2);
   myServo3.attach(3);
   myServo4.attach(4);
-  //myServo1.attach(5);
-  //myServo2.attach(6);
-  //myServo3.attach(7);
-  //myServo4.attach(8);
-  //myservo5
+  myServo5.attach(5);
+  myServo6.attach(6);
+  myServo7.attach(7);
+  myServo8.attach(8);
+ 
 }
 
-void loop()
-{
-  //rotations from 0 to 180 degrees
-  for (angle=0; angle < maxAngle; angle++)
-  {
-    for (i=0; i<8; i++){
-      array[i].write(angle);
-    }
-    delay(10);
-  }
-  //rotations from 180 back to 0 degrees
-  for (angle=maxAngle; angle > 0; angle--)
-  {
-    for (i=0; i<8; i++){
-      array[i].write(angle);
-    }
-    delay(10);
-  }
+void loop() {
+  forward();
+  delay(2000);
+  reverse();
+  delay(2000);
 }
 
+void forward(){
+  for (i=0; i<4; i++){
+      lefthandarray[i].write(0);
+   }
+  for (i=0; i<4; i++){
+      righthandarray[i].write(180);
+   }
+}
+ 
+void reverse(){
+ for (i=0; i<4; i++){
+      lefthandarray[i].write(180);
+   }
+  for (i=0; i<4; i++){
+      righthandarray[i].write(0);
+   }
+}
+
+ void rightTurn(){
+    return
+ }
+
+ void leftTurn(){
+    return
+ }
+
+ void stopRobot(){
+    return
+ } 
